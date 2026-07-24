4615b49fbfd422e4ab230275d65dbd8976f9b392
saurabh2059 <aryalsaurav141@gmail.com>
2026-07-24 21:18:27 +0545

    auto test

--- selenium test/test_worker_search.py
+++ selenium test/test_worker_search.py
@@ -4,18 +4,26 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select, WebDriverWait
 
 
+# opens chrome browser 
 driver = webdriver.Chrome()
+# creats wait object that wait upto 10 sec
 wait = WebDriverWait(driver, 10)
 
 try:
+	# loads the login page
 	driver.get("https:/gig-work.me/login")
 
+	# ec ensure waiting  until something happen
+	# until email located on page
+	# types keys in the input field
 	wait.until(EC.presence_of_element_located((By.NAME, "email"))).send_keys(
 		"sabinrai@gmail.com"
 	)
+	# by specifies how to find the element on page
 	driver.find_element(By.NAME, "password").send_keys("test@9824")
 	driver.find_element(By.XPATH, "//button[contains(., 'Sign In')]").click()
 
+	# wait until login succeed
 	wait.until(EC.url_contains("/customer/dashboard"))
 
 	driver.get("http://gig-work.me/customer/search-workers")
@@ -36,4 +44,6 @@ try:
 
 	print("Worker search successful")
 finally:
+
+	# close the browser
 	driver.quit()
